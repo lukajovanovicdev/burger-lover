630594a18f23efc3adc5d257f743370766c13352
andjelajankovic <andjela.jankovic@bosch.com>
2021-11-19 11:35:16 +0100

    changes

---
+++ src/colors.js
@@ -0,0 +1,7 @@
+export default {
+  red: "#bf0426",
+  yellow: "#bf9341",
+  gold: "#8c6c45",
+  brown: "#8c3030",
+  black: "#0d0d0d",
+};
--- src/components/buttons/ButtonsBase.js
+++ src/components/buttons/ButtonsBase.js
@@ -1,113 +1,116 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom'
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import "./ButtonBase.css";
+import colors from "../../colors";
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: 'relative',
+  position: "relative",
   height: 200,
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
     height: 100,
   },
-  '&:hover, &.Mui-focusVisible': {
+  "&:hover, &.Mui-focusVisible": {
     zIndex: 1,
-    '& .MuiImageBackdrop-root': {
+    "& .MuiImageBackdrop-root": {
       opacity: 0.15,
+      color: colors.gold,
     },
-    '& .MuiImageMarked-root': {
+    "& .MuiImageMarked-root": {
       opacity: 0,
     },
-    '& .MuiTypography-root': {
-      border: '4px solid currentColor',
+    "& .MuiTypography-root": {
+      border: "4px solid colors.gold",
+      backgroundColor: colors.gold,
+      opacity: "0.75",
     },
   },
 }));
 
-const ImageSrc = styled('span')({
-  position: 'absolute',
+const ImageSrc = styled("span")({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
 });
 
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: colors.black,
 }));
 
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundColor: theme.palette.common.black,
   opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
+  transition: theme.transitions.create("opacity"),
 }));
 
-const ImageMarked = styled('span')(({ theme }) => ({
+const ImageMarked = styled("span")(({ theme }) => ({
   height: 3,
   width: 18,
-  backgroundColor: theme.palette.common.white,
-  position: 'absolute',
+  position: "absolute",
   bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
 }));
 
 export default function ButtonBases() {
-    const navigate = useNavigate();
-    
-    const openIngredients =  () => {
-        navigate('/burger-lover/AddIngredients');
-    }
+  const navigate = useNavigate();
 
-    
-    const openBurgers = () => {
-        navigate('/burger-lover/Burgers')
-    }
-    const openCustomBurger = () => {
-        alert('openCustomBurger action');
-    }
+  const openIngredients = () => {
+    navigate("/burger-lover/AddIngredients");
+  };
 
-    const images = [
-        {
-          url: 'static/images/ingredients.jpg',
-          title: 'Ingredients',
-          width: '30%',
-          action: openIngredients,
-        },
-        {
-          url: 'static/images/burgers.jpg',
-          title: 'Burgers',
-          width: '40%',
-          action: openBurgers,
-        },
-        {
-          url: 'static/images/custom.jpg',
-          title: 'Make Custom Burger',
-          width: '30%',
-          action: openCustomBurger,
-        },
-      ];
-    
+  const openBurgers = () => {
+    navigate("/burger-lover/Burgers");
+  };
+  const openCustomBurger = () => {
+    alert("openCustomBurger action");
+  };
+
+  const images = [
+    {
+      url: "static/images/ingr.png",
+      title: "Ingredients",
+      width: "30%",
+      action: openIngredients,
+    },
+    {
+      url: "static/images/burger.png",
+      title: "Burgers",
+      width: "40%",
+      action: openBurgers,
+    },
+    {
+      url: "static/images/custom.jpg",
+      title: "Make Custom Burger",
+      width: "30%",
+      action: openCustomBurger,
+    },
+  ];
+
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+    <Box
+      sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+    >
       {images.map((image) => (
         <ImageButton
           focusRipple
@@ -125,7 +128,7 @@ export default function ButtonBases() {
               variant="subtitle1"
               color="inherit"
               sx={{
-                position: 'relative',
+                position: "relative",
                 p: 4,
                 pt: 2,
                 pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
