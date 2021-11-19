52fd7185c0b4c399ede4de0db7ccc4ae0059a4be
miroslavnikolic <miroslav.nikolic123456@gmail.com>
2021-11-19 11:51:54 +0100

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -7,41 +7,14 @@ import Burgers from './components/Burgers';
 import AddBurger from './components/AddBurger';
 import MakeCustomBurger from './components/MakeCustomBurger';
 import ButtonBases from './components/buttons/ButtonsBase';
+import useIngrediantsFetch from './services/useIngrediantsFetch';
 
 function App() {
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredient, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchIngredientHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://6195607a74c1bd00176c6d1f.mockapi.io/ingredients');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-      const data = await response.json();
-
-      const loadedIngredients = [];
-
-      for (const key in data) {
-        loadedIngredients.push({
-          id: key,
-          name: data[key].name,
-          tag: data[key].tag,
-          calories: data[key].calories,
-        });
-      }
-      setIngredients(loadedIngredients);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-  useEffect(() => {
-    fetchIngredientHandler();
-  }, [fetchIngredientHandler]);
+  const ingData = useIngrediantsFetch();
 
   async function addIngredientHandler(ingredient) {
     const response = await fetch('https://6195607a74c1bd00176c6d1f.mockapi.io/ingredients', {
@@ -54,8 +27,18 @@ function App() {
     const data = await response.json();
     console.log(data);
   }
+  let content = <p>No ingredient found</p>;
+
+  if (ingredient.length > 0) {
+    content = <Ingredients ingredient={ingredient} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
   return (
-<<<<<<< HEAD
     <div>
       
       <BrowserRouter>
@@ -73,26 +56,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/burger-lover/Ingredients"
-          element={<Ingredients data={ingredients} />}
-        />
-        <Route
-          path="/burger-lover/AddIngredients"
-          element={<AddIngredients onAddIngredient={addIngredientHandler} />}
-        />
-        <Route path="/burger-lover/Burgers" element={<Burgers />} />
-        <Route path="/burger-lover/AddBurger" element={<AddBurger />} />
-        <Route
-          path="*"
-          element={<Navigate to="/burger-lover/AddIngredients"></Navigate>}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
->>>>>>> 7bca16db112d641c78760bd675d8815f4cae7d57
   );
 }
 
